f0ad1406c3d68946ed8cf3bdb14e2c62e3a1af94
Dmitry Korochkin <abramslost@gmail.com>
2022-11-22 21:13:35 +0300

    loader

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -7,8 +7,9 @@ const Discover = () => {
   const {data, isFetching, error} = useGetTopChartsQuery();
 
   const genreTitle = 'Pop';
-  console.log(data);
-
+  
+  if(isFetching) return <Loader title="Loading songs..."/>;
+  if(error) return <Error />;
 
   return (
     <div>
@@ -30,7 +31,7 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
